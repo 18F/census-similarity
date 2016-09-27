1986e51f5ec63149a957ba628edfc8531a2cd6c3
CM Lubinski <christopher.lubinski@gsa.gov>
2016-09-27 13:37:15 +0000

    Refactor subcommands to be top-level commands
    Adds a setup.py file to give us entry points during installation; this allows
    us to execute commands directly, once the environment is installed.

--- census_similarity/commands/__init__.py
+++ census_similarity/commands/__init__.py
@@ -1,3 +1,8 @@
-from .cluster_by_field import cluster_by_field
-from .group_by import group_by
-from .lookup import lookup
+import logging
+
+from .cluster_by_field import cluster_by_field  # noqa
+from .group_by import group_by  # noqa
+from .lookup import lookup  # noqa
+
+
+logging.basicConfig(level=logging.DEBUG)
